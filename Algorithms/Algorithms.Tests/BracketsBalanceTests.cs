8de282d054a1343ec0cb533864f52d188ebd73ae
Marcus Weaver <onemangd@gmail.com>
2018-07-06 22:57:20 +0300

    Brackets Balance Task

---
+++ Algorithms/Algorithms.Tests/BracketsBalanceTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class BracketsBalanceTests
+    {
+        [Theory]
+        [InlineData("(()){()", false)]
+        [InlineData("(()){()}", true)]
+        [InlineData("[[[(()){}]{}]]", true)]
+        [InlineData("(((())()", false)]
+        public void Check(string input, bool expected)
+        {
+            Assert.Equal(expected, BracketsBalance.IsValidBracketString(input));
+        }
+    }
+}
---
+++ Algorithms/Algorithms/BracketsBalance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public static class BracketsBalance
+    {
+        private static readonly HashSet<char> OpeningBrackets = new HashSet<char>{'{','(','['};
+        
+        public static bool IsValidBracketString(string input)
+        {
+            var stack = new Stack<char>();
+
+            var bracketPairs = new Dictionary<char, char>
+            {
+                {'}', '{'},
+                {')', '('},
+                {']', '['}
+            };
+
+            foreach (var charick in input)
+            {
+                if (charick.IsOpeningBracket())
+                {
+                    stack.Push(charick);
+                    continue;
+                }
+
+                var lastBracket = stack.Peek();
+                
+                if (bracketPairs[charick] != lastBracket) return false;
+                
+                stack.Pop();
+            }
+            
+            return stack.Count == 0;
+        }
+
+        private static bool IsOpeningBracket(this char charick)
+        {
+            return OpeningBrackets.Contains(charick);
+        }
+    }
+}
