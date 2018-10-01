6d9f2f71aae673a5784ab7e1979b824b2667a8e0
Zohaib Sharani <zohaib.sharani@gmail.com>
2018-10-02 00:50:00 +0500

    Added Trie and it's unit test. Fixing issue https://github.com/Sharpach/Algorithms/issues/5

---
+++ Algorithms/Algorithms.Tests/Trie/TrieTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using Algorithms.Trie;
+
+namespace Algorithms.Tests
+{
+    public class TrieTests
+    {
+        [Fact]
+        public void Check()
+        {
+            var trie = new Algorithms.Trie.Trie();
+            trie.Insert("Hello");
+            trie.Insert("Hey");
+            Assert.Empty(trie.FindPrefix("h"));
+            Assert.Equal(2, trie.FindPrefix("H").Count);
+            trie.Insert("Hello");
+            trie.Insert("Hey");
+            Assert.Empty(trie.FindPrefix("h"));
+            Assert.Equal(2, trie.FindPrefix("H").Count);
+            trie.Insert("hello");
+            trie.Insert("hey");
+            Assert.Equal(2, trie.FindPrefix("h").Count);
+            Assert.Equal(2, trie.FindPrefix("H").Count);
+            Assert.Equal(2, trie.FindPrefix("He").Count);
+            Assert.Single(trie.FindPrefix("Hel"));
+            Assert.Single(trie.FindPrefix("Hey"));
+            trie.Clear();
+            Assert.Empty(trie.FindPrefix("h"));
+            Assert.Empty(trie.FindPrefix("H"));
+        }
+    }
+}
