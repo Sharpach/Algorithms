41bae0b4ad0ed1445985d200ea6871fb16b02000
Billy Wolfington <wolfington98@gmail.com>
2018-09-30 21:49:21 -0700

    Some classes for the BST implementation

---
+++ Algorithms/Algorithms/BinarySearchTree.cs
@@ -0,0 +1,37 @@
+using Algorithms.Models;
+using System;
+
+namespace Algorithms
+{
+    public class BinarySearchTree<T>
+    {
+        public BinarySearchTree()
+        {
+        }
+
+        public BinarySearchTree(T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BinarySearchTree(BinaryTreeNode<T> root)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
