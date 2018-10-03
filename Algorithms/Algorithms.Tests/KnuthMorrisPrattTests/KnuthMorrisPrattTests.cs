d2952b1703d2e08bc85ae91620403aa8e2557cae
Zohaib Sharani <mzhsarani@lmkr.com>
2018-10-03 10:39:01 +0500

    Updated unit test to use Theory and inlinedata.

--- Algorithms/Algorithms.Tests/KnuthMorrisPrattTests/KnuthMorrisPrattTests.cs
+++ Algorithms/Algorithms.Tests/KnuthMorrisPrattTests/KnuthMorrisPrattTests.cs
@@ -1,24 +1,30 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using Xunit;
 
 namespace Algorithms.Tests
 {
     public class KnuthMorrisPrattTests
     {
-        [Fact]
-        public void FunctionalityCheck()
+        [Theory]
+        [InlineData("AKMPAKMPCAADAKMPAKMPAA", "AKMPA", 3, 0, 0)]
+        [InlineData("AKMPAKMPCAADAKMPAKMPAA", "AKMPA", 3, 1, 12)]
+        [InlineData("AKMPAKMPCAADAKMPAKMPAA", "AKMPA", 3, 2, 16)]
+        [InlineData("AKMPACAADAKMPKMP", "PKMP", 1, 0, 12)]
+        public void FunctionalityCheck(string text, string search, int resultCount, int index, int expectedResult)
         {
-            var result = KnuthMorrisPratt.KnuthMorrisPratt.KMPSearch("AKMPAKMPCAADAKMPAKMPAA", "AKMPA");
+            var result = KnuthMorrisPratt.KnuthMorrisPratt.KMPSearch(text, search);
             Assert.NotEmpty(result);
-            Assert.Equal(3, result.Count);
-            Assert.Equal(0, result[0]);
-            Assert.Equal(12, result[1]);
-            Assert.Equal(16, result[2]);
+            Assert.Equal(resultCount, result.Count);
+            Assert.Equal(expectedResult, result[index]);
         }
 
-        [Fact]
-        public void EmptyCheck()
+        [Theory]
+        [InlineData("AKMPACAADAKMPKMP", "ASD")]
+        [InlineData("AKMPACAADAKMPKMP", "AADC")]
+        [InlineData("AKMPACAADAKMPKMP", "AADAKMPKP")]
+        public void EmptyCheck(string text, string search)
         {
-            var result = KnuthMorrisPratt.KnuthMorrisPratt.KMPSearch("AKMPACAADAKMPKMP", "ASD");
+            var result = KnuthMorrisPratt.KnuthMorrisPratt.KMPSearch(text, search);
             Assert.Empty(result);
         }
     }
