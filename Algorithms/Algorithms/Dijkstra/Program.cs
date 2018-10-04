bd38f0f63b4915057ad19b69eddb60b1c004aa8a
Henri <henri.duhamel@devinci.fr>
2018-10-04 22:42:41 +0200

    Adding the main forgot it

---
+++ Algorithms/Algorithms/Dijkstra/Program.cs
@@ -0,0 +1,9 @@
+public class Program
+    {
+        static void Main(string[] args)
+        {
+            var graph = Dijkstra.SetupExampleGraph();
+            var result = graph.GetShortestPath('1', '5');
+            Dijkstra.PathPrinter(result);
+        }
+    }
