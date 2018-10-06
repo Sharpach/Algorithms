a70198a1124d95eaed0e87a397b42a6cc8fce9a3
mdg357 <mdg357@gmail.com>
2018-10-06 16:37:35 -0500

    Implements issue #40 by adding Splay Tree class and tests

---
+++ Algorithms/Algorithms.Tests/SplayTreeTests/SplayTreeTests.cs
@@ -0,0 +1,149 @@
+﻿using Algorithms.SplayTree;
+using Xunit;
+
+namespace Algorithms.Tests.SplayTreeTests
+{
+    public class SplayTreeTests
+    {
+        private readonly int[] _treeValues = {50, 20, 80, 10, 30, 70, 90};
+        
+        [Fact]
+        public void GivenNumberList_ThenCorrectTreeBuild()
+        {
+            // Arrange
+            var tree = SplayTree<int>.BuildTree(_treeValues);
+
+            // Assert
+            Assert.Equal(50, tree.Value);
+            Assert.Equal(20, tree.Left.Value);
+            Assert.Equal(10, tree.Left.Left.Value);
+            Assert.Equal(30, tree.Left.Right.Value);
+            Assert.Equal(10, tree.Left.Left.Value);
+            Assert.Equal(80, tree.Right.Value);
+            Assert.Equal(70, tree.Right.Left.Value);
+            Assert.Equal(90, tree.Right.Right.Value);
+        }
+
+        [Fact]
+        public void GivenSearch_WhenValueIsAtRoot_ThenRootIsUnchanged()
+        {
+            // Arrange
+            var tree = SplayTree<int>.BuildTree(_treeValues);
+
+            // Act
+            tree = SplayTree<int>.Splay(tree, 50);
+
+            // Assert
+            Assert.Equal(50, tree.Value);
+        }
+
+        [Fact]
+        public void GivenSearch_WhenValueIsLeftChildOfRoot_ThenTreeRotated()
+        {
+            // Arrange
+            var tree = SplayTree<int>.BuildTree(_treeValues);
+
+            // Act
+            tree = SplayTree<int>.Splay(tree, 20);
+
+            // Assert
+            Assert.Equal(20, tree.Value);
+            Assert.Equal(10, tree.Left.Value);
+            Assert.Equal(50, tree.Right.Value);
+        }
+
+        [Fact]
+        public void GivenSearch_WhenValueIsRightChildOfRoot_ThenTreeRotated()
+        {
+            // Arrange
+            var tree = SplayTree<int>.BuildTree(_treeValues);
+
+            // Act
+            tree = SplayTree<int>.Splay(tree, 80);
+
+            // Assert
+            Assert.Equal(80, tree.Value);
+            Assert.Equal(50, tree.Left.Value);
+            Assert.Equal(90, tree.Right.Value);
+        }
+
+        [Fact]
+        public void GivenSearch_WhenZigZig_ThenTreeRotated()
+        {
+            // Arrange
+            var tree = SplayTree<int>.BuildTree(_treeValues);
+
+            // Act
+            tree = SplayTree<int>.Splay(tree, 10);
+
+            // Assert
+            Assert.Equal(10, tree.Value);
+        }
+
+        [Fact]
+        public void GivenSearch_WhenZagZag_ThenTreeRotated()
+        {
+            // Arrange
+            var tree = SplayTree<int>.BuildTree(_treeValues);
+
+            // Act
+            tree = SplayTree<int>.Splay(tree, 90);
+
+            // Assert
+            Assert.Equal(90, tree.Value);
+        }
+
+        [Fact]
+        public void GivenSearch_WhenZigZag_ThenTreeRotated()
+        {
+            // Arrange
+            var tree = SplayTree<int>.BuildTree(_treeValues);
+
+            // Act
+            tree = SplayTree<int>.Splay(tree, 20);
+
+            // Assert
+            Assert.Equal(20, tree.Value);
+        }
+
+        [Fact]
+        public void GivenSearch_WhenZagZig_ThenTreeRotated()
+        {
+            // Arrange
+            var tree = SplayTree<int>.BuildTree(_treeValues);
+
+            // Act
+            tree = SplayTree<int>.Splay(tree, 70);
+
+            // Assert
+            Assert.Equal(70, tree.Value);
+        }
+
+        [Fact]
+        public void GivenSearch_WhenValueNotInTree_ThenClosestNodeReturned()
+        {
+            // Arrange
+            var tree = SplayTree<int>.BuildTree(_treeValues);
+
+            // Act
+            tree = SplayTree<int>.Splay(tree, 100);
+
+            // Assert
+            Assert.Equal(90, tree.Value);
+        }
+
+        [Fact]
+        public void GivenSearch_WhenValueInTree_ThenNodeReturned()
+        {
+            // Arrange
+            var nodeValues = new[] {100, 50, 40, 30, 20, 200};
+            var tree = SplayTree<int>.BuildTree(nodeValues);
+
+            // Act
+            tree = SplayTree<int>.Splay(tree, 20);
+
+            // Assert
+            Assert.Equal(20, tree.Value);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/SplayTree/SplayTree.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.SplayTree
+{
+    /// <summary>
+    /// A generic implementation of the Splay Tree algorithm.
+    /// For more information and examples for this algorithm, 
+    /// see https://www.geeksforgeeks.org/splay-tree-set-1-insert/
+    /// </summary>
+    /// <typeparam name="T">A type that implements IComparable</typeparam>
+    public class SplayTree<T> where T : IComparable
+    {
+        /// <summary>
+        /// Create a Tree from the given list of values.
+        /// </summary>
+        /// <param name="list">The list of values to insert into the tree</param>
+        /// <param name="parent">The parent node</param>
+        /// <returns>The newly created Node</returns>
+        public static Node BuildTree(IReadOnlyCollection<T> list, Node parent = null)
+        {
+            if (list.Count == 0) return null;
+
+            var node = new Node
+            {
+                Value = list.First()
+            };
+
+            var leftList = list.Skip(1).Where(w => w.CompareTo(node.Value) < 0).ToList();
+            var rightList = list.Skip(1).Where(w => w.CompareTo(node.Value) > 0).ToList();
+
+            node.Left = BuildTree(leftList, node);
+            node.Right = BuildTree(rightList, node);
+
+            return node;
+        }
+
+        /// <summary>
+        /// Search the given tree for the provided search value.
+        /// </summary>
+        /// <param name="root">The root Node of the tree to search</param>
+        /// <param name="searchValue">The value for which to search</param>
+        /// <returns>If present, the Node containing the search value, otherwise, the last Node searched</returns>
+        public static Node Splay(Node root, T searchValue)
+        {
+            if (root == null || root.Value.CompareTo(searchValue) == 0)
+            {
+                return root;
+            }
+
+            return root.Value.CompareTo(searchValue) > 0
+                ? TraverseLeftSubTree(root, searchValue)
+                : TraverseRightSubTree(root, searchValue);
+        }
+
+        #region Private Methods
+
+        /// <summary>
+        /// Traverse through the right sub-tree of the given tree, looking for the search value.
+        /// </summary>
+        /// <param name="root">The root Node of the sub-tree to search</param>
+        /// <param name="searchValue">The value for which to search</param>
+        /// <returns>If no right Node exists, return the root. Otherwise, the root of the possibly rotated sub-tree</returns>
+        private static Node TraverseRightSubTree(Node root, T searchValue)
+        {
+            if (root.Right == null) return root;
+
+            if (root.Right.Value.CompareTo(searchValue) > 0)
+            {
+                root.Right.Left = Splay(root.Right.Left, searchValue);
+
+                if (root.Right.Left != null)
+                {
+                    root.Right = RightRotate(root.Right);
+                }
+            }
+            else if (root.Right.Value.CompareTo(searchValue) < 0)
+            {
+                root.Right.Right = Splay(root.Right.Right, searchValue);
+                root = LeftRotate(root);
+            }
+
+            return root.Right == null ? root : LeftRotate(root);
+        }
+
+        /// <summary>
+        /// Traverse through the left sub-tree of the given tree, looking for the search value.
+        /// </summary>
+        /// <param name="root">The root Node of the sub-tree to search</param>
+        /// <param name="searchValue">The value for which to search</param>
+        /// <returns>If no left Node exists, return the root. Otherwise, the root of the possibly rotated sub-tree</returns>
+        private static Node TraverseLeftSubTree(Node root, T searchValue)
+        {
+            if (root.Left == null) return root;
+
+            if (root.Left.Value.CompareTo(searchValue) > 0)
+            {
+                root.Left.Left = Splay(root.Left.Left, searchValue);
+                root = RightRotate(root);
+            }
+            else if (root.Left.Value.CompareTo(searchValue) < 0)
+            {
+                root.Left.Right = Splay(root.Left.Right, searchValue);
+
+                if (root.Left.Right != null)
+                {
+                    root.Left = LeftRotate(root.Left);
+                }
+            }
+
+            return root.Left == null ? root : RightRotate(root);
+        }
+
+        /// <summary>
+        /// Left rotate sub-tree rooted with x.
+        /// </summary>
+        /// <param name="x">Node around which to rotate</param>
+        /// <returns>The new root of the tree</returns>
+        private static Node LeftRotate(Node x)
+        {
+            var y = x.Right;
+            x.Right = y.Left;
+            y.Left = x;
+            return y;
+        }
+
+        /// <summary>
+        /// Right rotate sub-tree rooted with x.
+        /// </summary>
+        /// <param name="x">Node around which to rotate</param>
+        /// <returns>The new root of the tree</returns>
+        private static Node RightRotate(Node x)
+        {
+            var y = x.Left;
+            x.Left = y.Right;
+            y.Right = x;
+            return y;
+        }
+
+        #endregion
+
+        #region Classes
+
+        public class Node
+        {
+            public T Value { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+        }
+
+        #endregion
+    }
+}
