748d8b07fe69bc21e32b18edd09474447e80cb60
drake-vcu <42615139+drake-vcu@users.noreply.github.com>
2018-10-07 18:55:35 -0500

    Adding HashTable algorithm implementation

---
+++ Algorithms/Algorithms/HashTable/IHashTableCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace Algorithms
+{
+    internal abstract class IHashTableCollection : ICollection
+    {
+        internal HashTable _hashtable;
+
+        internal IHashTableCollection(HashTable hashtable)
+        {
+            _hashtable = hashtable;
+        }
+
+        public abstract void CopyTo(Array array, int arrayIndex);
+
+        internal void ValidateArray(Array array, int arrayIndex)
+        {
+            if (array == null)
+                throw new ArgumentNullException("array");
+            if (array.Rank != 1)
+                throw new ArgumentException("Array Multi Dimension Not Supported");
+            if (arrayIndex < 0)
+                throw new ArgumentOutOfRangeException("arrayIndex");
+            if (array.Length - arrayIndex < _hashtable.Count)
+                throw new ArgumentException("Array too small");
+        }
+
+        public abstract IEnumerator GetEnumerator();
+
+        public virtual bool IsSynchronized { get => _hashtable.IsSynchronized; }
+
+        public virtual Object SyncRoot { get => _hashtable.SyncRoot; }
+
+        public virtual int Count { get => _hashtable.Count; }
+    }
+}
---
+++ Algorithms/Algorithms/HashTable/ValueCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace Algorithms
+{
+    internal class ValueCollection : IHashTableCollection
+    {
+        internal ValueCollection(HashTable hashtable) : base(hashtable) { }
+
+        public override void CopyTo(Array array, int arrayIndex)
+        {
+            ValidateArray(array, arrayIndex);
+            _hashtable.CopyValues(array, arrayIndex);
+        }
+
+        public override IEnumerator GetEnumerator() => new HashTableEnumerator(_hashtable, HashTableEnumerator.ObjectReturnType.Values);
+    }
+}
