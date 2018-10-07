9d4921b81a41a77d187277630ac607e6e8c9a6d4
Valdas <valdas771@gmail.com>
2018-10-07 13:07:48 +0300

    #36: implement n-ary tree

---
+++ Algorithms/Algorithms.Tests/NTreeTests/NTreeTests.cs
@@ -0,0 +1,50 @@
+﻿using Algorithms.NTree;
+using Xunit;
+
+namespace Algorithms.Tests.NTreeTests
+{
+    public class NTreeTests
+    {
+        NTree<int> BuildTree()
+        {
+            var tree = new NTree<int>(0);
+            tree.Add(1).Add(2).Add(3);
+                tree.Children[0].Add(4).Add(5);
+                    tree.Children[0].Children[0].Add(6);
+                tree.Children[1].Add(7);
+                tree.Children[2].Add(8);
+                    tree.Children[2].Children[0].Add(9);
+            return tree;
+        }
+
+        [Fact]
+        public void ElementsAreInsertedInCorrectOrder()
+        {
+            var tree = BuildTree();
+
+            var result = tree.ToString();
+
+            Assert.Equal("0146527389", result);
+        }
+
+        [Fact]
+        public void ParentReturnsCorrectNode()
+        {
+            var tree = BuildTree();
+            var child = tree.Children[1].Children[0];
+            var parent = tree.Children[1];
+
+            var result = child.Parent;
+
+            Assert.Equal(parent.Value, result.Value);
+        }
+
+        [Fact]
+        public void ParentOfRootIsNull()
+        {
+            var tree = BuildTree();
+
+            Assert.Null(tree.Parent);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/NTree/NTree.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.NTree
+{
+    public class NTree<T>
+    {
+        public T Value { get; set; }
+        public NTree<T> Parent { get; set; }
+        public List<NTree<T>> Children { get; set; } = new List<NTree<T>>();
+
+        public NTree(T value)
+        {
+            Value = value;
+        }
+
+        public NTree<T> Add(T value)
+        {
+            var child = new NTree<T>(value)
+            {
+                Parent = this
+            };
+            Children.Add(child);
+            return this;
+        }
+        
+        // returns concatenated values in prefix order
+        public override string ToString()
+        {
+            return Value + string.Concat(Children.Select(x => x.ToString()));
+        }
+    }
+}
