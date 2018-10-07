45574b6ea21cddc8b95562037fc610638af9621c
Leefrost <evertodante@gmail.com>
2018-10-07 11:30:56 +0300

    #35 AStar
    Provided A* search on abstract nodes. Added 2D matrix as a playground in tests

---
+++ Algorithms/Algorithms.Tests/AStarTests/TestField/Matrix.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Algorithms.AStar;
+
+namespace Algorithms.Tests.AStarTests.TestField
+{
+    public class MatrixField
+    {
+        public MatrixNode[][] Matrix;
+
+        public int Width { get { return Matrix.Length; } }
+        public int Height { get { return Matrix[0].Length; } }
+
+        public MatrixNode Start;
+        public MatrixNode Goal;
+
+        /// <example>
+        /// |S W|
+        /// |W  |
+        /// |W F|
+        /// </example>
+        public MatrixField()
+        {
+            Start = new MatrixNode(this, 0, 0, false);
+            Goal = new MatrixNode(this, 2, 2, false);
+
+            Matrix = new MatrixNode[3][];
+            for (var i = 0; i < 3; i++)
+                Matrix[i] = new MatrixNode[3];
+
+            Matrix[Start.X][Start.Y] = Start;
+            Matrix[Goal.X][Goal.Y] = Goal;
+
+            Matrix[0][1] = new MatrixNode(this, 0, 2, false);
+            Matrix[0][2] = new MatrixNode(this, 0, 2, true);
+
+            Matrix[1][0] = new MatrixNode(this, 1, 0, true);
+            Matrix[1][1] = new MatrixNode(this, 1, 0, false);
+            Matrix[1][2] = new MatrixNode(this, 1, 0, false);
+
+            Matrix[2][0] = new MatrixNode(this, 2, 0, true);
+            Matrix[2][1] = new MatrixNode(this, 2, 1, true);
+        }
+
+        public string Print(IEnumerable<IWayNode> path)
+        {
+            var output = "";
+            for (var i = 0; i < Width; i++)
+            {
+                for (var j = 0; j < Height; j++)
+                {
+                    output += Matrix[i][j].Print(Start, Goal, path);
+                }
+                output += "\n";
+            }
+            return output;
+        }
+    }
+}
---
+++ Algorithms/Algorithms/AStar/AStar.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.AStar
+{
+    public enum State
+    {
+        Searching,
+        GoalFound,
+        Failed
+    }
+
+    internal class DuplicateComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            return x <= y ? -1 : 1;
+        }
+    }
+
+    public class AStarSearch
+    {
+        private readonly SortedList<int, IWayNode> _openNodes;
+        private readonly SortedList<int, IWayNode> _closeNodes;
+
+        public IEnumerable<IWayNode> OpenNodes => _openNodes.Values;
+        public IEnumerable<IWayNode> ClosedNodes => _closeNodes.Values;
+
+        public IWayNode Current { get; private set; }
+        public IWayNode Goal { get; private set; }
+
+        public int Steps { get; private set; }
+
+        public AStarSearch(IWayNode start, IWayNode goal)
+        {
+            var duplicateComparer = new DuplicateComparer();
+            _openNodes = new SortedList<int, IWayNode>(duplicateComparer);
+            _closeNodes = new SortedList<int, IWayNode>(duplicateComparer);
+
+            Reset(start, goal);
+        }
+
+        public void Reset(IWayNode start, IWayNode goal)
+        {
+            _openNodes.Clear();
+            _closeNodes.Clear();
+
+            Current = start;
+            Goal = goal;
+
+            _openNodes.Add(Current);
+            Current.SetOpenNode(true);
+        }
+
+        public State Run()
+        {
+            while (true)
+            {
+                var state = NextStep();
+                if (state != State.Searching)
+                    return state;
+            }
+        }
+
+        private State NextStep()
+        {
+            Steps++;
+            while (true)
+            {
+                if (_openNodes.IsEmpty())
+                {
+                    return State.Failed;
+                }
+
+                Current = _openNodes.Pop();
+                if (Current.IsClosedNodes(ClosedNodes))
+                {
+                    continue;
+                }
+
+                break;
+            }
+
+            Current.SetOpenNode(false);
+            _closeNodes.Add(Current);
+            Current.SetClosedNode(true);
+
+            if (Current.IsGoal(Goal))
+            {
+                return State.GoalFound;
+            }
+
+            foreach (var child in Current.Children)
+            {
+                if (child.IsOpenNodes(OpenNodes) || child.IsClosedNodes(ClosedNodes))
+                {
+                    continue;
+                }
+
+                child.Parent = Current;
+                child.SetMovementCost(Current);
+                child.SetEstimatedCost(Goal);
+
+                _openNodes.Add(child);
+                child.SetOpenNode(true);
+            }
+
+            return State.Searching;
+        }
+
+        public IEnumerable<IWayNode> GetPath()
+        {
+            if (Current != null)
+            {
+                var next = Current;
+                var path = new List<IWayNode>();
+                while (next != null)
+                {
+                    path.Add(next);
+                    next = next.Parent;
+                }
+                path.Reverse();
+                return path.ToArray();
+            }
+            return null;
+        }
+    }
+}
