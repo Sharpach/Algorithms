09527a43521e222ea0d9abe8428c6258bd6abe36
Leefrost <evertodante@gmail.com>
2018-10-07 21:32:37 +0300

    #35 AStar
    Provided simple realization for A* algo. Added 2d Matrix playground for test purposes.

---
+++ Algorithms/Algorithms.Tests/AStarTests/AStarTests.cs
@@ -0,0 +1,29 @@
+﻿using Algorithms.AStar;
+using Algorithms.Tests.AStarTests.TestField;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.AStarTests
+{
+    public class AStarTests
+    {
+        [Fact]
+        void AStar_RunOnStable3On3Filed_ReturnCorrectPath()
+        {
+            /* The way
+             * |F.W|
+             * |W..|
+             * |WWF|
+             */
+
+            var matrix = new MatrixField();
+            var search = new AStarSearch(matrix.Start, matrix.Goal);
+            var expectedPath = "s.W\nW..\nWWg\n";
+
+            search.Run();
+            var foundPath = matrix.Print(search.GetPath());
+
+            foundPath.Should().Be(expectedPath);
+        }
+    }
+}
