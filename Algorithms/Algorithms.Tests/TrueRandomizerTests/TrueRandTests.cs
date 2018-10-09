08b15835a9809216dc5a78c0f169d061c5de9a24
Stevan Marjanovic <stevan@stevit.rs>
2018-10-09 23:45:11 +0200

    Added true randomized method

---
+++ Algorithms/Algorithms.Tests/TrueRandomizerTests/TrueRandTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Algorithms.Tests.TrueRandomizerTests
+{
+	public class TrueRandomizerTests
+    {
+        [Fact]
+		public void TrueRandomizerTest()
+        {
+			int[] input = { 1, 10000 };
+			TrueRandomizer.Program randomizer = new TrueRandomizer.Program(input[0], input[1]);
+			bool output = randomizer.RandomNumber >= input[0] && randomizer.RandomNumber <= input[1];
+			Assert.True(output);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/TrueRandomizer/TrueRandomizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+
+namespace Algorithms.TrueRandomizer
+{
+    public class Program
+    {
+		public int RandomNumber;
+
+		protected static int GetRandomNumber(int min, int max) {
+			string uri = $"https://www.random.org/integers/?num=1&min={min}&max={max}&col=1&base=10&format=plain&rnd=new";         
+			WebClient client = new WebClient();
+			var content = client.DownloadString(uri);
+			int randomNumber = Int32.Parse(content);
+			return randomNumber;
+
+		}
+
+		public Program(int min, int max) {
+			RandomNumber = GetRandomNumber(min, max);
+		}
+    }
+}
